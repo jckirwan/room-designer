bb807864d926e4408d33cd75d47cb3ce8edbd238
jean-claude.kirwan <jean-claude.kirwan@owllabs.com>
2022-11-16 12:26:03 -0500

    Add Tailwind, PostCSS and OUI... and some fonts.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
+import "./style.css";
 
 const rootElement = document.getElementById('container');
 const root = createRoot(rootElement);
